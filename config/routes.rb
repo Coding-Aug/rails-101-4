5adb38591d0551da61b263ee12670cc3b8c7cce8
augustus <einsteinbeing@hotmail.com>
2017-09-05 11:15:58 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groups do
 
+
     member do
       post :join
       post :quit
@@ -10,5 +11,11 @@ Rails.application.routes.draw do
 
     resources :posts
   end
+
+  namespace :account do
+    resources :groups
+
+  end
+
   root 'groups#index'
 end
